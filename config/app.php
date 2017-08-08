5837f2250f07599d1639c0b5cc2ae4633e002989
Ferry Stephanus Suwita <ferry.stephanus.s@gmail.com>
2017-08-08 14:44:53 +0700

    - Remove mongo

--- config/app.php
+++ config/app.php
@@ -177,9 +177,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-      
-        /* MongoDB */
-        Jenssegers\Mongodb\MongodbServiceProvider::class,
     ],
 
     /*
