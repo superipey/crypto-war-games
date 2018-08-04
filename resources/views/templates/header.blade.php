df2aff38286580f4f0eef33b4f54e75513821a36
Ferry Stephanus Suwita <ferry.stephanus.s@gmail.com>
2018-08-04 13:03:13 +0700

    fix link

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -45,7 +45,7 @@
     <nav class="navbar navbar-static-top">
       <div class="container">
         <div class="navbar-header">
-          <a href="{{ asset('assets') }}/index2.html" class="navbar-brand"><b>Crypto</b>WarGames</a>
+          <a href="{{ url('/') }}" class="navbar-brand"><b>Crypto</b>WarGames</a>
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
             <i class="fa fa-bars"></i>
           </button>
