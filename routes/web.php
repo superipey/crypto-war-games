b7a2ceef947152ffd6886ac3b51de7f5d227ed1a
Ferry Stephanus Suwita <ferry.stephanus.s@gmail.com>
2018-08-29 20:38:45 +0700

    fix team

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,40 @@ Route::group(['middleware'=>['auth:web']], function() {
 
     Route::get('/create_soal', 'HomeController@soal');
 });
+
+Route::get("/teamlist", function() {
+    $teams = \App\Team::all();
+    echo "<table>";
+    echo "  <tr>";
+    echo "    <th>No</th>";
+    echo "    <th>Kelas</th>";
+    echo "    <th>Nama Team</th>";
+    echo "    <th>Anggota</th>";
+    echo "    <th>Status Cipher</th>";
+    echo "  </tr>";
+    $i = 1;
+    foreach ($teams as $team) {
+        echo "<tr>";
+        echo "  <td>".$i++."</td>";
+        $members = $team->members;
+        $name = [];
+        $cipher = [];
+        $kelas = '';
+        foreach ($members as $player) {
+            if (!empty($player->cipher)) $cipher[] = $player->cipher->id;
+            $name[] = $player->name;
+            $kelas = $player->kelas;
+        }
+        $name = implode("<br>", $name);
+        echo "  <td>$kelas</td>";
+        echo "  <td>$team->team_name</td>";
+        echo "  <td>$name</td>";
+
+        $cipher = implode("<br>", $cipher);
+
+        echo "  <td>".(!empty($cipher) ? 'OK':'Revisi')."</td>";
+
+        echo "</tr>";
+    }
+    echo "</table>";
+});
